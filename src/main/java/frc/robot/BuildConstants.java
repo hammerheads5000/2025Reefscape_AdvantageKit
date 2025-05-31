e6a2e660220e07a4eba1264b5463e57a3d448707
Tori <ndtech260@gmail.com>
2025-05-31 12:23:54 -0400

    Update at 'Sat May 31 12:23:53 EDT 2025'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025Reefscape_AdvantageKit";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 35;
-    public static final String GIT_SHA = "db84da0b006681052f8fd385bb050e438fcbd764";
-    public static final String GIT_DATE = "2025-05-31 10:06:59 EDT";
+    public static final int GIT_REVISION = 36;
+    public static final String GIT_SHA = "362389d5e0d98c97b5938570addc49510a6836f7";
+    public static final String GIT_DATE = "2025-05-31 10:45:44 EDT";
     public static final String GIT_BRANCH = "event_mastatechamps";
-    public static final String BUILD_DATE = "2025-05-31 10:07:00 EDT";
-    public static final long BUILD_UNIX_TIME = 1748700420375L;
+    public static final String BUILD_DATE = "2025-05-31 10:45:44 EDT";
+    public static final long BUILD_UNIX_TIME = 1748702744423L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -587,7 +587,7 @@ public class Constants {
 
         public static final Angle GRAB_ANGLE = Rotations.of(0.22);
         public static final Angle MAX_CLIMB_ANGLE = Rotations.of(0.53);
-        public static final Angle RESET_ANGLE = Rotations.of(0.59);
+        public static final Angle RESET_ANGLE = Rotations.of(0.60);
 
         public static final TalonFXConfiguration CLIMB_CONFIGS = new TalonFXConfiguration()
                 .withMotorOutput(OUTPUT_CONFIGS)
@@ -725,7 +725,7 @@ public class Constants {
         public static final Distance DISTANCE_TO_REEF = Inches.of(29.0 / 2).plus(Dimensions.BUMPER_THICKNESS);
         public static final Distance DISTANCE_TO_PROCESSOR = Inches.of(29.0 / 2).plus(Dimensions.BUMPER_THICKNESS);
 
-        public static final Distance APPROACH_DISTANCE = Inches.of(24); // *extra* distance to reef when
+        public static final Distance APPROACH_DISTANCE = Inches.of(30); // *extra* distance to reef when
         // approaching
         public static final Distance PULL_DISTANCE = Inches.of(8);
         public static final Distance STAGE1_DEPLOY_DISTANCE = Inches.of(10);

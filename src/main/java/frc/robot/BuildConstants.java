23d37a4d8cd729b7c625185e6b833b52391ae52f
LordOfFrogs <ndtech260@gmail.com>
2025-06-15 18:13:16 -0400

    Squashed commit of the following:
    commit 0f1e4a6e4e54cc036678fddd046fec755321382f
    Author: Tori <ndtech260@gmail.com>
    Date:   Sun Jun 15 18:13:01 2025 -0400
    
        slightly changed l4 height
    
    commit 7d65ce1d713433ce7247856ae5e1842f4aa38f87
    Author: Tori <ndtech260@gmail.com>
    Date:   Sat May 31 12:25:03 2025 -0400
    
        Update at 'Sat May 31 12:25:03 EDT 2025'
    
    commit e6a2e660220e07a4eba1264b5463e57a3d448707
    Author: Tori <ndtech260@gmail.com>
    Date:   Sat May 31 12:23:54 2025 -0400
    
        Update at 'Sat May 31 12:23:53 EDT 2025'
    
    commit 362389d5e0d98c97b5938570addc49510a6836f7
    Author: Tori <ndtech260@gmail.com>
    Date:   Sat May 31 10:45:44 2025 -0400
    
        Update at 'Sat May 31 10:45:43 EDT 2025'
    
    commit db84da0b006681052f8fd385bb050e438fcbd764
    Author: Tori <ndtech260@gmail.com>
    Date:   Sat May 31 10:06:59 2025 -0400
    
        Update at 'Sat May 31 10:06:59 EDT 2025'
    
    commit 57f7fb17f63dc37af61c3cc550b9c08f52d10633
    Author: Tori <ndtech260@gmail.com>
    Date:   Sat May 31 09:31:40 2025 -0400
    
        Update at 'Sat May 31 09:31:40 EDT 2025'
    
    commit 8c6fa4887cadf1e2387f650ec09ffe5dc5565eab
    Author: Tori <ndtech260@gmail.com>
    Date:   Sat May 31 08:32:34 2025 -0400
    
        Update at 'Sat May 31 08:32:33 EDT 2025'
    
    commit 28dfd6eec2921e437ed60fe2bee3f656fa74e3a1
    Author: Tori <ndtech260@gmail.com>
    Date:   Fri May 30 20:13:07 2025 -0400
    
        Update at 'Fri May 30 20:13:07 EDT 2025'
    
    commit 51b0a9179b3a5546047ca6893d992f27ae8ef2d9
    Author: Tori <ndtech260@gmail.com>
    Date:   Fri May 30 20:11:07 2025 -0400
    
        Update at 'Fri May 30 20:11:07 EDT 2025'
    
    commit 56dabf655b8f4258466b57295ddf010a5784b552
    Author: Tori <ndtech260@gmail.com>
    Date:   Fri May 30 20:05:46 2025 -0400
    
        Update at 'Fri May 30 20:05:46 EDT 2025'
    
    commit 387d7f25a98f6e500a611fcb02108a47af20a89f
    Author: Tori <ndtech260@gmail.com>
    Date:   Fri May 30 19:57:58 2025 -0400
    
        Update at 'Fri May 30 19:57:57 EDT 2025'

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025Reefscape_AdvantageKit";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 27;
-    public static final String GIT_SHA = "daa364fff73769b1560499feca0d1d1cfc116e3a";
-    public static final String GIT_DATE = "2025-05-29 13:29:13 EDT";
-    public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-05-30 09:47:37 EDT";
-    public static final long BUILD_UNIX_TIME = 1748612857012L;
-    public static final int DIRTY = 1;
+    public static final int GIT_REVISION = 38;
+    public static final String GIT_SHA = "7d65ce1d713433ce7247856ae5e1842f4aa38f87";
+    public static final String GIT_DATE = "2025-05-31 12:25:03 EDT";
+    public static final String GIT_BRANCH = "event_mastatechamps";
+    public static final String BUILD_DATE = "2025-05-31 12:25:04 EDT";
+    public static final long BUILD_UNIX_TIME = 1748708704172L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }

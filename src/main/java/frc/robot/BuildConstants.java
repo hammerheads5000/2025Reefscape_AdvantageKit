0554490c40121670b57ea2866814bf3fd30ed10a
LordOfFrogs <ndtech260@gmail.com>
2025-06-17 00:43:11 -0400

    full visualization

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025Reefscape_AdvantageKit";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 38;
-    public static final String GIT_SHA = "7d65ce1d713433ce7247856ae5e1842f4aa38f87";
-    public static final String GIT_DATE = "2025-05-31 12:25:03 EDT";
-    public static final String GIT_BRANCH = "event_mastatechamps";
-    public static final String BUILD_DATE = "2025-05-31 12:25:04 EDT";
-    public static final long BUILD_UNIX_TIME = 1748708704172L;
-    public static final int DIRTY = 0;
+    public static final int GIT_REVISION = 31;
+    public static final String GIT_SHA = "aca31fa36fba125399559c8c11fd4128410f5d21";
+    public static final String GIT_DATE = "2025-06-15 18:13:56 EDT";
+    public static final String GIT_BRANCH = "Strats";
+    public static final String BUILD_DATE = "2025-06-15 23:18:10 EDT";
+    public static final long BUILD_UNIX_TIME = 1750043890603L;
+    public static final int DIRTY = 1;
 
     private BuildConstants() {}
 }

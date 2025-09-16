67549686a41a7b881c59834c7e70a7e033279467
logandelaar1 <delaarlogan@gmail.com>
2025-09-15 20:58:02 -0400

    please jsut updateeeeee

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,13 +5,13 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025Reefscape_AdvantageKit";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 40;
-    public static final String GIT_SHA = "ba4fd8b355cc0002f2556d13ecd80b9da11a218e";
-    public static final String GIT_DATE = "2025-09-15 20:47:59 EDT";
+    public static final int GIT_REVISION = 41;
+    public static final String GIT_SHA = "438968b3966693a33eb3ea18e4b31eb42d3f8148";
+    public static final String GIT_DATE = "2025-09-15 20:57:16 EDT";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-09-15 20:50:37 EDT";
-    public static final long BUILD_UNIX_TIME = 1757983837500L;
-    public static final int DIRTY = 1;
+    public static final String BUILD_DATE = "2025-09-15 20:57:37 EDT";
+    public static final long BUILD_UNIX_TIME = 1757984257299L;
+    public static final int DIRTY = 0;
 
     private BuildConstants() {}
 }

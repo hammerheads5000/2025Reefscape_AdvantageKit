438968b3966693a33eb3ea18e4b31eb42d3f8148
logandelaar1 <delaarlogan@gmail.com>
2025-09-15 20:57:16 -0400

    maybe this will work

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025Reefscape_AdvantageKit";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 38;
-    public static final String GIT_SHA = "1e0ca288f4027db1fe07c3d1074a9ec2c0ecf73f";
-    public static final String GIT_DATE = "2025-09-15 17:14:19 EDT";
+    public static final int GIT_REVISION = 40;
+    public static final String GIT_SHA = "ba4fd8b355cc0002f2556d13ecd80b9da11a218e";
+    public static final String GIT_DATE = "2025-09-15 20:47:59 EDT";
     public static final String GIT_BRANCH = "main";
-    public static final String BUILD_DATE = "2025-09-15 20:41:25 EDT";
-    public static final long BUILD_UNIX_TIME = 1757983285550L;
+    public static final String BUILD_DATE = "2025-09-15 20:50:37 EDT";
+    public static final long BUILD_UNIX_TIME = 1757983837500L;
     public static final int DIRTY = 1;
 
     private BuildConstants() {}
--- src/main/java/frc/robot/commands/AutoCoralCommand.java
+++ src/main/java/frc/robot/commands/AutoCoralCommand.java
@@ -136,9 +136,8 @@ public class AutoCoralCommand extends SequentialCommandGroup {
 
         Translation2d directionToCoral = toCoral.div(toCoral.getNorm());
         Translation2d approachTranslation =
-                coralField.plus(directionToCoral.times(IntakeConstants.CORAL_APPROACH_OFFSET.in(Meters)));
-        Rotation2d approachRotation =
-                directionToCoral.getAngle().plus(Rotation2d.kPi); // rear faces coral
+                coralField.minus(directionToCoral.times(IntakeConstants.CORAL_APPROACH_OFFSET.in(Meters)));
+        Rotation2d approachRotation = directionToCoral.getAngle().plus(Rotation2d.kPi); // rear faces coral
 
         return Optional.of(new Pose2d(approachTranslation, approachRotation));
     }

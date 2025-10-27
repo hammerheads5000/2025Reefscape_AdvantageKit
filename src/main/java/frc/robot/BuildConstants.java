3367b7be5d3602c98e8b78729c73248805aec050
Tori <mx.tori.dell@gmail.com>
2025-10-27 02:39:49 -0400

    Final buildconstants update

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -5,12 +5,12 @@ public final class BuildConstants {
     public static final String MAVEN_GROUP = "";
     public static final String MAVEN_NAME = "2025Reefscape_AdvantageKit";
     public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 148;
-    public static final String GIT_SHA = "52d729c0244fe0c11c12225bef34525f4d45429b";
-    public static final String GIT_DATE = "2025-10-26 16:17:10 EDT";
+    public static final int GIT_REVISION = 149;
+    public static final String GIT_SHA = "39254ba832511b8dc06b437cfc459edc6d6ce485";
+    public static final String GIT_DATE = "2025-10-26 16:18:36 EDT";
     public static final String GIT_BRANCH = "event_cki";
-    public static final String BUILD_DATE = "2025-10-26 16:17:15 EDT";
-    public static final long BUILD_UNIX_TIME = 1761509835876L;
+    public static final String BUILD_DATE = "2025-10-26 16:18:39 EDT";
+    public static final long BUILD_UNIX_TIME = 1761509919135L;
     public static final int DIRTY = 0;
 
     private BuildConstants() {}
